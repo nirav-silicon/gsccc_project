f3894d6f1803aec20f02880b742f485b86153b19
Riya <riyashrivastav@siliconithub.com>
2023-03-17 16:04:30 +0530

    dfdsfsdfsdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
     </head>
     <body class="antialiased">
 
-        <h3>hbgfhfhhgfjjhelllogfgfdhgfdhdfhdgh</h3>
+        <h3>fdfsdfsdfdsfsdfsdhbgfhfhhgfjjhelllogfgfdhgfdhdfhdgh</h3>
 
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
